2f159919bee82cae27b1f4ec07e93ca41b05182e
2eonardo <leonardo.soldani3@edu.unifi.it>
2026-04-14 15:58:50 +0200

    Pulizia codice

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,123 +2,115 @@ import React from 'react';
 import './Footer.css';
 
 export default function Footer() {
-    const iconPlaceholder = "https://placehold.co/16x16/777/white?text=i";
+    const icon = "https://placehold.co/16x16/777/white?text=i";
 
     return (
         <footer className="main-footer" aria-label="Sitemap">
             <div className="footer-container">
                 <div className="footer-grid">
-                    {/* COLONNA 1 */}
                     <section className="footer-column">
                         <h2 className="footer-title">LOREM IPSUM SERVICES</h2>
                         <ul className="footer-list">
-                            <li><a href="#">Lorem ipsum dolor</a></li>
-                            <li><a href="#">Sit amet consectetur</a></li>
-                            <li><a href="#">Adipiscing elit sed</a></li>
-                            <li><a href="#">Eiusmod tempor</a></li>
-                            <li><a href="#">Incididunt ut labore</a></li>
-                            <li><a href="#">Dolore magna aliqua</a></li>
+                            <li><a href="/lorem-ipsum-dolor">Lorem ipsum dolor</a></li>
+                            <li><a href="/sit-amet-consectetur">Sit amet consectetur</a></li>
+                            <li><a href="/adipiscing-elit-sed">Adipiscing elit sed</a></li>
+                            <li><a href="/eiusmod-tempor">Eiusmod tempor</a></li>
+                            <li><a href="/incididunt-ut-labore">Incididunt ut labore</a></li>
+                            <li><a href="/dolore-magna-aliqua">Dolore magna aliqua</a></li>
                         </ul>
                         <h2 className="footer-title second-title">LOREM GUIDES</h2>
                         <ul className="footer-list">
-                            <li><a href="#">Minim veniam quis</a></li>
-                            <li><a href="#">Nostrud exercitation</a></li>
-                            <li><a href="#">Ullamco laboris nisi</a></li>
-                            <li><a href="#">Ut aliquip ex ea</a></li>
-                            <li><a href="#">Commodo consequat</a></li>
+                            <li><a href="/minim-veniam-quis">Minim veniam quis</a></li>
+                            <li><a href="/nostrud-exercitation">Nostrud exercitation</a></li>
+                            <li><a href="/ullamco-laboris-nisi">Ullamco laboris nisi</a></li>
+                            <li><a href="/ut-aliquip-ex-ea">Ut aliquip ex ea</a></li>
+                            <li><a href="/commodo-consequat">Commodo consequat</a></li>
                         </ul>
                     </section>
 
-                    {/* COLONNA 2 */}
                     <section className="footer-column">
                         <h2 className="footer-title">LOREM INTERNATIONAL</h2>
                         <ul className="footer-list">
-                            <li><a href="#">Duis aute irure</a></li>
-                            <li><a href="#">Dolor in reprehenderit</a></li>
-                            <li><a href="#">Voluptate velit esse</a></li>
-                            <li><a href="#">Cillum dolore eu</a></li>
-                            <li><a href="#">Fugiat nulla pariatur</a></li>
-                            <li><a href="#">Excepteur sint</a></li>
-                            <li><a href="#">Occaecat cupidatat</a></li>
+                            <li><a href="/duis-aute-irure">Duis aute irure</a></li>
+                            <li><a href="/dolor-in-reprehenderit">Dolor in reprehenderit</a></li>
+                            <li><a href="/voluptate-velit-esse">Voluptate velit esse</a></li>
+                            <li><a href="/cillum-dolore-eu">Cillum dolore eu</a></li>
+                            <li><a href="/fugiat-nulla-pariatur">Fugiat nulla pariatur</a></li>
+                            <li><a href="/excepteur-sint">Excepteur sint</a></li>
+                            <li><a href="/occaecat-cupidatat">Occaecat cupidatat</a></li>
                         </ul>
                         <h2 className="footer-title second-title">LOREM PARTNERS</h2>
                         <ul className="footer-list">
-                            <li><a href="#">Non proident sunt</a></li>
-                            <li><a href="#">Culpa qui officia</a></li>
-                            <li><a href="#">Deserunt mollit</a></li>
+                            <li><a href="/non-proident-sunt">Non proident sunt</a></li>
+                            <li><a href="/culpa-qui-officia">Culpa qui officia</a></li>
+                            <li><a href="/deserunt-mollit">Deserunt mollit</a></li>
                         </ul>
                     </section>
 
-                    {/* COLONNA 3 */}
                     <section className="footer-column">
                         <h2 className="footer-title">LOREM GROUP SITES</h2>
                         <ul className="footer-list">
-                            <li><a href="#">Anim id est laborum</a></li>
-                            <li><a href="#">Sed ut perspiciatis</a></li>
-                            <li><a href="#">Unde omnis iste</a></li>
-                            <li><a href="#">Natus error sit</a></li>
-                            <li><a href="#">Voluptatem accusantium</a></li>
-                            <li><a href="#">Doloremque laudantium</a></li>
-                            <li><a href="#">Totam rem aperiam</a></li>
-                            <li><a href="#">Eaque ipsa quae</a></li>
+                            <li><a href="/anim-id-est-laborum">Anim id est laborum</a></li>
+                            <li><a href="/sed-ut-perspiciatis">Sed ut perspiciatis</a></li>
+                            <li><a href="/unde-omnis-iste">Unde omnis iste</a></li>
+                            <li><a href="/natus-error-sit">Natus error sit</a></li>
+                            <li><a href="/voluptatem-accusantium">Voluptatem accusantium</a></li>
+                            <li><a href="/doloremque-laudantium">Doloremque laudantium</a></li>
+                            <li><a href="/totam-rem-aperiam">Totam rem aperiam</a></li>
+                            <li><a href="/eaque-ipsa-quae">Eaque ipsa quae</a></li>
                         </ul>
                     </section>
 
-                    {/* COLONNA 4 */}
                     <section className="footer-column-special">
-                        <div className="sub-section">
-                            <h2 className="footer-title">LOREM NEWSLETTER</h2>
-                            <div className="app-links">
-                                <a href="#"><img src={iconPlaceholder} alt="" />Lorem ipsum newsletter</a>
-                            </div>
+                        <h2 className="footer-title">LOREM NEWSLETTER</h2>
+                        <a href="/lorem-ipsum-newsletter" className="flex-link"><img src={icon} alt="" />Lorem ipsum newsletter</a>
+
+                        <hr className="footer-sep" aria-hidden="true" />
+
+                        <h2 className="footer-title">LOREM MOBILE APPS</h2>
+                        <div className="flex-link">
+                            <img src={icon} alt="" />
+                            <a href="/lorem-ios">Lorem iOS</a>
+                            <div className="divider" aria-hidden="true"></div>
+                            <a href="/lorem-android">Lorem Android</a>
                         </div>
-                        <hr className="footer-sep" />
-                        <div className="sub-section">
-                            <h2 className="footer-title">LOREM MOBILE APPS</h2>
-                            <div className="app-links">
-                                <img src={iconPlaceholder} alt="" />
-                                <a href="#">Lorem iOS</a>
-                                <div className="divider"></div>
-                                <a href="#">Lorem Android</a>
-                            </div>
-                        </div>
-                        <hr className="footer-sep" />
-                        <div className="sub-section">
-                            <h2 className="footer-title">LOREM SUBSCRIPTION</h2>
-                            <ul className="footer-list-icons">
-                                <li> <a href="#"><img src={iconPlaceholder} alt="" />Lorem Archives</a></li>
-                                <li> <a href="#"><img src={iconPlaceholder} alt="" />Lorem Subscribe</a></li>
-                                <li> <a href="#"><img src={iconPlaceholder} alt="" />Lorem Connect</a></li>
-                                <li> <a href="#"><img src={iconPlaceholder} alt="" />Lorem Journal</a></li>
-                                <li><a href="#">Lorem Events</a></li>
-                                <li><a href="#">Lorem Contact</a></li>
-                            </ul>
-                        </div>
+
+                        <hr className="footer-sep" aria-hidden="true" />
+
+                        <h2 className="footer-title">LOREM SUBSCRIPTION</h2>
+                        <ul className="footer-list-icons">
+                            <li><a href="/lorem-archives" className="flex-link"><img src={icon} alt="" />Lorem Archives</a></li>
+                            <li><a href="/lorem-subscribe" className="flex-link"><img src={icon} alt="" />Lorem Subscribe</a></li>
+                            <li><a href="/lorem-connect" className="flex-link"><img src={icon} alt="" />Lorem Connect</a></li>
+                            <li><a href="/lorem-journal" className="flex-link"><img src={icon} alt="" />Lorem Journal</a></li>
+                            <li><a href="/lorem-events">Lorem Events</a></li>
+                            <li><a href="/lorem-contact">Lorem Contact</a></li>
+                        </ul>
                     </section>
                 </div>
 
-                <hr className="footer-sep" />
-                <div className="title-centered">
-                    <span className="footer-title ">LOREM LEGAL INFO</span>
+                <hr className="footer-sep" aria-hidden="true" />
+                <nav className="centered-section" aria-label="Legal info">
+                    <span className="footer-title">LOREM LEGAL INFO</span>
                     <ul className="legal-links">
-                        <li><a href="#">Lorem Legal</a></li>
-                        <li><a href="#">Privacy Lorem</a></li>
-                        <li><a href="#">Cookie Lorem</a></li>
-                        <li><a href="#">Terms Lorem</a></li>
-                        <li><a href="#">Help FAQ</a></li>
+                        <li><a href="/lorem-legal">Lorem Legal</a></li>
+                        <li><a href="/privacy-lorem">Privacy Lorem</a></li>
+                        <li><a href="/cookie-lorem">Cookie Lorem</a></li>
+                        <li><a href="/terms-lorem">Terms Lorem</a></li>
+                        <li><a href="/help-faq">Help FAQ</a></li>
                     </ul>
-                </div>
+                </nav>
 
-                <hr className="footer-sep" />
-                <div className="title-centered">
+                <hr className="footer-sep" aria-hidden="true" />
+                <section className="centered-section" aria-label="Social">
                     <span className="footer-title">FOLLOW LOREM</span>
                     <div className="social-links">
-                         <a href="#"><img src={iconPlaceholder} alt="" />Facebook</a>
-                         <a href="#"><img src={iconPlaceholder} alt="" />Youtube</a>
-                         <a href="#"><img src={iconPlaceholder} alt="" />Instagram</a>
-                         <a href="#"><img src={iconPlaceholder} alt="" />TikTok</a>
+                        <a href="/facebook" className="flex-link"><img src={icon} alt="" />Facebook</a>
+                        <a href="/youtube" className="flex-link"><img src={icon} alt="" />Youtube</a>
+                        <a href="/instagram" className="flex-link"><img src={icon} alt="" />Instagram</a>
+                        <a href="/tiktok" className="flex-link"><img src={icon} alt="" />TikTok</a>
                     </div>
-                </div>
+                </section>
             </div>
         </footer>
     );
