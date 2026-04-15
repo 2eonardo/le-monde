09c074fd8e932a28c0ded577933baa538c9f472c
2eonardo <leonardo.soldani3@edu.unifi.it>
2026-04-15 17:09:10 +0200

    HeaderResponsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,6 +17,7 @@ export default function Header() {
                         <img src="https://placehold.co/40x40/666666/ffffff?text=S" alt="" aria-hidden="true" />
                         <span>Services</span>
                     </button>
+
                 </div>
                 {/* CENTRO: Logo principale */}
                 <div className="center-header">
@@ -51,6 +52,18 @@ export default function Header() {
                 <a href="/search" className="nav-button" aria-label="Search">
                     <img src="https://placehold.co/40x40/666666/ffffff?text=S" alt="" aria-hidden="true" />
                 </a>
+
+                {/* Sezione a comparsa max-width 1024px:*/}
+                <div className="tablet-button">
+                    <button>
+                        <img src="https://placehold.co/40x40/666666/ffffff?text=V" alt="" aria-hidden="true" />
+                        <span>Videò</span>
+                    </button>
+                    <button>
+                        <img src="https://placehold.co/40x40/666666/ffffff?text=E" alt="" aria-hidden="true" />
+                        <span>En continu</span>
+                    </button>
+                </div>
 
                 <div className="menu-divider" aria-hidden="true"></div>
 
